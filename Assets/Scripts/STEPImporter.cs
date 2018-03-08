62afb118eed191ca429b117be21a400720601e53
Andreas Oven Aalsaunet <andreas.aalsaunet@gmail.com>
2018-03-08 15:04:19 +0100

    Changed how the geometrical data is passed from the DLL to Unity. Now the indices have their own separate int buffer and are no longer casted to float to be included with the rest

--- Assets/Scripts/STEPImporter.cs
+++ Assets/Scripts/STEPImporter.cs
@@ -10,23 +10,24 @@ public class STEPImporter : MonoBehaviour {
 
 	public string stepFilePath;
 	public bool debugStatements;
-	IntPtr dataBufferPtr = IntPtr.Zero;
+	IntPtr geometricalBufferPtr = IntPtr.Zero;
+	IntPtr indexBufferPtr = IntPtr.Zero;
     Int32 vertexElementCount = 0;
 	Int32 normalElementCount = 0;
 	Int32 uv2ElementCount = 0;
 	Int32 triangleElementCount = 0;
 	
 	[DllImport("STEPImport")]
-	private static extern int Get3DGeometry(string filepath, ref IntPtr dataBufferPtr, ref Int32 vertexCount, ref Int32 normalCount, ref Int32 uv2Count, ref Int32 triangleCount);
+	private static extern int Get3DGeometry(string filepath, ref IntPtr geometricalBufferPtr, ref IntPtr indexBufferPtr, ref Int32 vertexCount, ref Int32 normalCount, ref Int32 uv2Count, ref Int32 triangleCount);
 
 	void Start () {
 		
 		System.Diagnostics.Stopwatch timer = (debugStatements) ? System.Diagnostics.Stopwatch.StartNew() : null;
 		
 		// Call the C++ dll with the file name
-		int returnCode = Get3DGeometry(stepFilePath, ref dataBufferPtr, ref vertexElementCount, ref normalElementCount, ref uv2ElementCount, ref triangleElementCount);
+		int returnCode = Get3DGeometry(stepFilePath, ref geometricalBufferPtr, ref indexBufferPtr, ref vertexElementCount, ref normalElementCount, ref uv2ElementCount, ref triangleElementCount);
 		
-		Debug.Log("Number of shapes: " + returnCode);
+		Debug.Log("Number of unique indices: " + returnCode);
 		// TODO: Check the return code
 
 		if (debugStatements) {
@@ -45,27 +46,26 @@ public class STEPImporter : MonoBehaviour {
 		float[] vertexBuffer = new float[vertexElementCount];
 		float[] normalBuffer = new float[normalElementCount];
 		float[] uv2Buffer = new float[uv2ElementCount];
-		float[] triangleBuffer = new float[triangleElementCount];
+		Int32[] triangleBuffer = new Int32[triangleElementCount];
 		
 		// Possible might want: (int)Math.Min(dataBufLength, (long)Int32.MaxValue)
 		//Int32[] stopIndices = new Int32[] {vertexElementCount, (vertexElementCount + normalElementCount), (vertexElementCount + normalElementCount + uv2ElementCount), (vertexElementCount + normalElementCount + uv2ElementCount + triangleElementCount)};
-		Int32[] startIndices = new Int32[] {0, vertexElementCount, (vertexElementCount + normalElementCount), (vertexElementCount + normalElementCount + uv2ElementCount)};
+		Int32[] startIndices = new Int32[] {0, vertexElementCount, (vertexElementCount + normalElementCount)};
 		try {
-			Marshal.Copy(new IntPtr(dataBufferPtr.ToInt32() + startIndices[0]), vertexBuffer, 0, vertexElementCount); 
-			Marshal.Copy(new IntPtr(dataBufferPtr.ToInt32() + startIndices[1]), normalBuffer, 0, normalElementCount); 
-			Marshal.Copy(new IntPtr(dataBufferPtr.ToInt32() + startIndices[2]), uv2Buffer, 0, uv2ElementCount); 
-			Marshal.Copy(new IntPtr(dataBufferPtr.ToInt32() + startIndices[3]), triangleBuffer, 0, triangleElementCount); 
-			// Marshal.Copy(dataBufferPtr, vertexBuffer, startIndices[0], vertexElementCount); 
-			// Marshal.Copy(dataBufferPtr, normalBuffer, startIndices[1], normalElementCount); 
-			// Marshal.Copy(dataBufferPtr, uv2Buffer, startIndices[2], uv2ElementCount); 
-			// Marshal.Copy(dataBufferPtr, triangleBuffer, startIndices[3], triangleElementCount); 
+			Marshal.Copy(new IntPtr(geometricalBufferPtr.ToInt32() + startIndices[0]), vertexBuffer, 0, vertexElementCount); 
+			Marshal.Copy(new IntPtr(geometricalBufferPtr.ToInt32() + startIndices[1]), normalBuffer, 0, normalElementCount); 
+			Marshal.Copy(new IntPtr(geometricalBufferPtr.ToInt32() + startIndices[2]), uv2Buffer, 0, uv2ElementCount); 
+			Marshal.Copy(indexBufferPtr, triangleBuffer, 0, triangleElementCount); 
 		}
 		finally {
-			Marshal.FreeHGlobal(dataBufferPtr);
+			Marshal.FreeHGlobal(geometricalBufferPtr);
+			Marshal.FreeHGlobal(indexBufferPtr);
 		}        
 
-		
+		foreach (Int32 index in triangleBuffer)
+			Debug.Log(index);
 
+		
 		Vector3[] vertices = new Vector3[vertexElementCount / 3];
 		for (int i = 0, j = 0; j < vertexElementCount; i++, j += 3)
 			vertices[i] = new Vector3(vertexBuffer[j], vertexBuffer[j + 1], vertexBuffer[j + 2]);
@@ -82,25 +82,28 @@ public class STEPImporter : MonoBehaviour {
 		mesh.vertices = vertices;
 		mesh.normals = normals;
 		mesh.uv2 = uv2s;
-
-		int[] triangleBuffer2 = new int[triangleBuffer.Length];
-		for (int i = 0; i < triangleBuffer.Length; i++) {		
-			triangleBuffer2[i] = (int)triangleBuffer[i];
-			Debug.Log(triangleBuffer2[i]);
-		}
-		mesh.triangles = Array.ConvertAll(triangleBuffer, x => (int)x);
+		mesh.triangles = triangleBuffer;
 
 		GameObject model = new GameObject(Path.GetFileName(stepFilePath));
 		MeshFilter meshFilter = model.AddComponent<MeshFilter>();
-		MeshRenderer meshRenderer = model.AddComponent<MeshRenderer>();
 		meshFilter.mesh = mesh;
+		model.AddComponent<MeshRenderer>();	
 		
 		Instantiate(model, Vector3.zero, Quaternion.identity);	
 
-		Debug.Log("Triangles" + meshFilter.mesh.triangles.Length);
+		Debug.Log("Triangles: " + meshFilter.mesh.triangles.Length);
 		if (debugStatements) {
 			Debug.Log("Time spent generating Unity meshes: " + timer.Elapsed.TotalSeconds + " seconds.");
 			timer.Reset();
 		}
+
+		// Int32[] triangleBuffer2 = new Int32[triangleBuffer.Length];
+		// for (int i = 0; i < triangleBuffer.Length; i++) {	
+		// 	Debug.Log("Index float value: " + triangleBuffer[i]);
+		// 	triangleBuffer2[i] = (Int32)triangleBuffer[i];
+		// 	Debug.Log("Index int value: " + triangleBuffer2[i]);
+		// }
+		// mesh.triangles = triangleBuffer2;
+		// //mesh.triangles = Array.ConvertAll(triangleBuffer, x => (int)x);
 	}
 }
