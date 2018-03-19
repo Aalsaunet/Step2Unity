d253fd3e735cfeda84da565b3e1fed85975542a2
Andreas Oven Aalsaunet <andreas.aalsaunet@gmail.com>
2018-03-19 14:50:09 +0100

    Updated the StepImporter script to follow the structure of the updated STEPImporter DLL

--- Assets/Scripts/STEPImporter.cs
+++ Assets/Scripts/STEPImporter.cs
@@ -11,23 +11,22 @@ public class STEPImporter : MonoBehaviour {
 	public string stepFilePath;
 	public bool debugStatements;
 	public Material defaultMaterial;
-	IntPtr geometricalBufferPtr = IntPtr.Zero;
-	IntPtr indexBufferPtr = IntPtr.Zero;
-    Int32 vertexElementCount = 0;
-	Int32 normalElementCount = 0;
-	Int32 uv2ElementCount = 0;
-	Int32 triangleElementCount = 0;
+	IntPtr vertexBuffer = IntPtr.Zero;
+	IntPtr indexBuffer = IntPtr.Zero;
+    Int32 vertexEntryCount = 0;
+	Int32 indexCount = 0;
 	
 	[DllImport("STEPImport")]
-	private static extern int Get3DGeometry(IntPtr stepFilePath, ref IntPtr geometricalBufferPtr, ref IntPtr indexBufferPtr, ref Int32 vertexCount, ref Int32 normalCount, ref Int32 uv2Count, ref Int32 triangleCount);
+	private static extern int ImportSTEPFile(IntPtr stepFilePath, ref IntPtr vertexBuffer, ref Int32 vertexEntryCount, ref IntPtr indexBuffer, ref Int32 indexCount);
 
 	void Start () {
 		
 		System.Diagnostics.Stopwatch timer = (debugStatements) ? System.Diagnostics.Stopwatch.StartNew() : null;
 		
 		string formattedFilePath = stepFilePath.Replace("\\", "\\\\");
+		
 		// Call the C++ dll with the file name
-		int returnCode = Get3DGeometry(Marshal.StringToHGlobalAnsi(stepFilePath), ref geometricalBufferPtr, ref indexBufferPtr, ref vertexElementCount, ref normalElementCount, ref uv2ElementCount, ref triangleElementCount);
+		int returnCode = ImportSTEPFile(Marshal.StringToHGlobalAnsi(stepFilePath), ref vertexBuffer, ref vertexEntryCount, ref indexBuffer, ref indexCount);
 		
 		// TODO: Check the return code
 
@@ -37,51 +36,32 @@ public class STEPImporter : MonoBehaviour {
 		}
 		if (debugStatements) {
 			Debug.Log("Time spent on transfering the data to managed C# float arrays: " + timer.Elapsed.TotalSeconds + " seconds.");
-			Debug.Log("Number of vertices: " + vertexElementCount / 3);
-			Debug.Log("Number of normals: " + normalElementCount / 3); 
-			Debug.Log("Number of uv2s: " + uv2ElementCount / 2); 
-			Debug.Log("Number of triangles: " + triangleElementCount / 3); 
+			Debug.Log("Number of vertices: " + vertexEntryCount / 3);
+			Debug.Log("Number of triangles: " + indexCount / 3); 
 			timer.Reset();
 		}
 			
-		float[] vertexBuffer = new float[vertexElementCount];
-		float[] normalBuffer = new float[normalElementCount];
-		float[] uv2Buffer = new float[uv2ElementCount];
-		int[] triangleBuffer = new int[triangleElementCount];
+		float[] managedVertexBuffer = new float[vertexEntryCount];
+		int[] managedIndexBuffer = new int[indexCount];
 		
-		Int32[] startIndices = new Int32[] {0, vertexElementCount, (vertexElementCount + normalElementCount)};
 		try {
-			Marshal.Copy(new IntPtr(geometricalBufferPtr.ToInt32() + startIndices[0]), vertexBuffer, 0, vertexElementCount); 
-			Marshal.Copy(new IntPtr(geometricalBufferPtr.ToInt32() + startIndices[1]), normalBuffer, 0, normalElementCount); 
-			Marshal.Copy(new IntPtr(geometricalBufferPtr.ToInt32() + startIndices[2]), uv2Buffer, 0, uv2ElementCount); 
-			Marshal.Copy(indexBufferPtr, triangleBuffer, 0, triangleElementCount); 
+			Marshal.Copy(vertexBuffer, managedVertexBuffer, 0, vertexEntryCount); 
+			Marshal.Copy(indexBuffer, managedIndexBuffer, 0, indexCount); 
 		}
 		finally {
-			Marshal.FreeHGlobal(geometricalBufferPtr);
-			Marshal.FreeHGlobal(indexBufferPtr);
+			Marshal.FreeHGlobal(vertexBuffer);
+			Marshal.FreeHGlobal(indexBuffer);
 		}        
 		
-		Vector3[] vertices = new Vector3[vertexElementCount / 3];
-		for (int i = 0, j = 0; j < vertexElementCount; i++, j += 3)
-			vertices[i] = new Vector3(vertexBuffer[j], vertexBuffer[j + 1], vertexBuffer[j + 2]);
-
-		Vector3[] normals = new Vector3[normalElementCount / 3];
-		for (int i = 0, j = 0; j < normalElementCount; i++, j += 3)
-			normals[i] = new Vector3(normalBuffer[j], normalBuffer[j + 1], normalBuffer[j + 2]);
-
-		Vector2[] uv2s = new Vector2[uv2ElementCount / 2];
-		for (int i = 0, j = 0; j < uv2ElementCount; i++, j += 2)
-			uv2s[i] = new Vector2(uv2Buffer[j], uv2Buffer[j + 1]);
-
-		Mesh mesh = new Mesh();
-		mesh.vertices = vertices;
-		mesh.normals = normals;
-		//mesh.uv2 = uv2s;
-		mesh.triangles = triangleBuffer;
+		Vector3[] vertices = new Vector3[vertexEntryCount / 3];
+		for (int i = 0, j = 0; j < vertexEntryCount; i++, j += 3)
+			vertices[i] = new Vector3(managedVertexBuffer[j], managedVertexBuffer[j + 1], managedVertexBuffer[j + 2]);
 
 		GameObject model = new GameObject(Path.GetFileName(stepFilePath));
+		
 		MeshFilter meshFilter = model.AddComponent<MeshFilter>();
-		meshFilter.mesh = mesh;
+		meshFilter.mesh.vertices = vertices;
+		meshFilter.mesh.triangles = managedIndexBuffer;
 		meshFilter.mesh.RecalculateNormals();
 
 		MeshRenderer meshRenderer = model.AddComponent<MeshRenderer>();	
