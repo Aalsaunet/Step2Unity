3241b31b7e001ee85bab797abf360d8f4f9eae1a
Andreas Oven Aalsaunet <andreas.aalsaunet@gmail.com>
2018-03-27 17:46:43 +0200

    Started using shared materials instead of instance ones. This reduces the draw calls by over 50% as it enabled a lot more dynamic batching. To use shared materials the color pallette code was removed and instead replaced by individual pre-colored materials

--- Assets/Scripts/STEPImporter.cs
+++ Assets/Scripts/STEPImporter.cs
@@ -21,23 +21,17 @@ public class STEPImporter : MonoBehaviour {
 	/* Unity arguments */
 	public string stepFilePath;
 	public bool debugStatements;
-	public Material defaultMaterial;
+	public Material[] defaultMaterials;
 	private GameObject rootObject;
 
 	/* Subshape variables */
 	private Int32 totalNumberOfSubShapes = 0;
-	
-	/* Color pallette variables */
-	private Color[] defaultColorPalette;
-	private const float MAX_RGB_VALUE = 255f;
 
 	/* Debug variables */
 	private uint totalVertexCount = 0;
 	private uint totalTriangleCount = 0;
 
 	void Start () {	
-		
-		PopulateColorPalettes();
 		System.Diagnostics.Stopwatch timer = (debugStatements) ? System.Diagnostics.Stopwatch.StartNew() : null;	
 		int returnCode = ImportSTEPFile(Marshal.StringToHGlobalAnsi(stepFilePath.Replace("\\", "\\\\")), ref totalNumberOfSubShapes);
 
@@ -94,60 +88,8 @@ public class STEPImporter : MonoBehaviour {
         meshFilter.mesh.RecalculateNormals();
 
         MeshRenderer meshRenderer = subobject.AddComponent<MeshRenderer>();
-        meshRenderer.material = defaultMaterial;
-        meshRenderer.material.color = defaultColorPalette[index % defaultColorPalette.Length];
-
+        meshRenderer.sharedMaterial = defaultMaterials[index % defaultMaterials.Length];
         totalVertexCount += (uint)vertices.Length;
         totalTriangleCount += (uint)(managedIndexBuffer.Length / 3);
     }
-
-	private void PopulateColorPalettes()
-    {
-
-        defaultColorPalette = new Color[17];
-        int i = 0;
-        // "50 shades of grey"
-		defaultColorPalette[i++] = new Color(146f / MAX_RGB_VALUE, 158f / MAX_RGB_VALUE, 148f / MAX_RGB_VALUE, 1f); // Middle grey (#929E94)
-        defaultColorPalette[i++] = new Color(130f / MAX_RGB_VALUE, 147f / MAX_RGB_VALUE, 153f / MAX_RGB_VALUE, 1f); // dark grey (#829399)
-        defaultColorPalette[i++] = new Color(88f / MAX_RGB_VALUE, 88f / MAX_RGB_VALUE, 88f / MAX_RGB_VALUE, 1f); // dark-grey (#585858)
-		defaultColorPalette[i++] = new Color(66f / MAX_RGB_VALUE, 75f / MAX_RGB_VALUE, 84f / MAX_RGB_VALUE, 1f); // black-grey (#424B54)
-		
-        // Blues 
-        defaultColorPalette[i++] = new Color(157f / MAX_RGB_VALUE, 217f / MAX_RGB_VALUE, 210f / MAX_RGB_VALUE, 1f); // Pale blue (#9DD9D2)
-        defaultColorPalette[i++] = new Color(64f / MAX_RGB_VALUE, 121f / MAX_RGB_VALUE, 140f / MAX_RGB_VALUE, 1f); // Sea blue (#40798C)
-		defaultColorPalette[i++] = new Color(111f / MAX_RGB_VALUE, 145f / MAX_RGB_VALUE, 159f / MAX_RGB_VALUE, 1f); // Mist blue (#6F919F)
-
-        // Greens
-        defaultColorPalette[i++] = new Color(214f / MAX_RGB_VALUE, 246f / MAX_RGB_VALUE, 221f / MAX_RGB_VALUE, 1f); // Pistachio green (#D6F6DD)
-        defaultColorPalette[i++] = new Color(182 / MAX_RGB_VALUE, 215f / MAX_RGB_VALUE, 185f / MAX_RGB_VALUE, 1f); // Light eagreen (#B6D7B9) 
-		defaultColorPalette[i++] = new Color(85 / MAX_RGB_VALUE, 144f / MAX_RGB_VALUE, 146f / MAX_RGB_VALUE, 1f); // Seagreen (#559092)
-        defaultColorPalette[i++] = new Color(192f / MAX_RGB_VALUE, 214f / MAX_RGB_VALUE, 132f / MAX_RGB_VALUE, 1f); // Spring green (#C0D684)
-        defaultColorPalette[i++] = new Color(125f / MAX_RGB_VALUE, 207f / MAX_RGB_VALUE, 182f / MAX_RGB_VALUE, 1f); // green (#7DCFB6)
-		defaultColorPalette[i++] = new Color(212f / MAX_RGB_VALUE, 218f / MAX_RGB_VALUE, 179f / MAX_RGB_VALUE, 1f); // Beige green (#D4DAB3)
-		defaultColorPalette[i++] = new Color(136f / MAX_RGB_VALUE, 159f / MAX_RGB_VALUE, 137f / MAX_RGB_VALUE, 1f); // Military green (#889F89)
-		defaultColorPalette[i++] = new Color(78f / MAX_RGB_VALUE, 107f / MAX_RGB_VALUE, 83f / MAX_RGB_VALUE, 1f); // Dark green (#4E6B53)
-		
-        // Yellows
-        defaultColorPalette[i++] = new Color(255f / MAX_RGB_VALUE, 244f / MAX_RGB_VALUE, 145f / MAX_RGB_VALUE, 1f); // Pale yellow (#FFF491)
-		defaultColorPalette[i++] = new Color(202f / MAX_RGB_VALUE, 193f / MAX_RGB_VALUE, 110f / MAX_RGB_VALUE, 1f); // Dark yellow (#CAC16E)
-    }
-
-		// int nextShapeIndexToProcess = 0;
-		// int threadCount = Environment.ProcessorCount;
-		// int shapesPerThread = (totalNumberOfSubShapes / threadCount);
-		// int leftOverShapes = (totalNumberOfSubShapes % threadCount);
-
-		// Thread[] availableThreads = new Thread[Environment.ProcessorCount];
-		// availableThreads[0] = new Thread(GetSubShape);
-		// availableThreads[0].Start(Enumerable.Range(nextShapeIndexToProcess, (shapesPerThread + leftOverShapes)).ToArray());
-		// nextShapeIndexToProcess += (shapesPerThread + leftOverShapes);
-
-		// for (int i = 1; i < availableThreads.Length; i++) {
-		// 	availableThreads[i] = new Thread(GetSubShape);
-		// 	availableThreads[i].Start(Enumerable.Range(nextShapeIndexToProcess, shapesPerThread).ToArray());
-		// 	nextShapeIndexToProcess += shapesPerThread;
-		// }
-
-		// for (int i = 0; i < availableThreads.Length; i++)
-		// 	availableThreads[i].Join();
 }
